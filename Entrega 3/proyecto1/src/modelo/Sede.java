f7478fab54da155c584e8a7ee830a462497860d3
Jeronimocifci <a.cifci@uniandes.edu.co>
2023-10-16 15:11:27 -0500

    sede/seguro

--- Entrega 3/proyecto1/src/modelo/Sede.java
+++ Entrega 3/proyecto1/src/modelo/Sede.java
@@ -1,5 +1,51 @@
 package modelo;
 
 public class Sede {
+    private String nombre;
+    private String ubicacion;
+    private String horarios;
 
+    // Constructor
+    public Sede(String nombre, String ubicacion, String horarios) {
+        this.nombre = nombre;
+        this.ubicacion = ubicacion;
+        this.horarios = horarios;
+    }
+
+    // Métodos para acceder a los atributos
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public String getHorarios() {
+        return horarios;
+    }
+
+    // Métodos para modificar los atributos (si es necesario)
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public void setHorarios(String horarios) {
+        this.horarios = horarios;
+    }
+
+    public static void main(String[] args) {
+        // Crear instancias de las sedes
+        Sede bogota = new Sede("Bogotá", "Cra. 11 #82-71", "08:00-17:00");
+        Sede cartagena = new Sede("Cartagena", "Cra. 8 #28-36", "08:00-17:00");
+        Sede medellin = new Sede("Medellín", "Cra. 13 #73-57", "08:00-17:00");
+
+        // Imprimir información de las sedes
+
+    }
 }
+
--- Entrega 3/proyecto1/src/modelo/Seguro.java
+++ Entrega 3/proyecto1/src/modelo/Seguro.java
@@ -1,5 +1,48 @@
 package modelo;
 
 public class Seguro {
+	
+    private String nombre;
+    private double tarifaDiaria;
 
+    public Seguro(String nombre, double tarifaDiaria) {
+        this.nombre = nombre;
+        this.tarifaDiaria = tarifaDiaria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getTarifaDiaria() {
+        return tarifaDiaria;
+    }
+
+    public void setTarifaDiaria(double tarifaDiaria) {
+        this.tarifaDiaria = tarifaDiaria;
+    }
+    
+    //metodos adic
+    public String getNombre0() {
+        return nombre;
+    }
+
+    public double getTarifa0() {
+        return tarifaDiaria;
+    }
+
+    public static void main(String[] args) {
+        // Crear instancias de SeguroSencillo y SeguroCompleto como objetos
+        Seguro seguroSencillo = new Seguro("SeguroSencillo", 10000.0);
+        Seguro seguroCompleto = new Seguro("SeguroCompleto", 20000.0);
+
+
+    }
 }
+
+
+
