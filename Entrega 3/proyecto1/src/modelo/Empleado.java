65bdec02aae8e581a002fbd7815018fffd264a79
MrRobot138 <123522773+MrRobot138@users.noreply.github.com>
2023-10-16 20:13:40 -0500

    Update Empleado.java

--- Entrega 3/proyecto1/src/modelo/Empleado.java
+++ Entrega 3/proyecto1/src/modelo/Empleado.java
@@ -1,5 +1,38 @@
 package modelo;
+import modelo.Sede;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Empleado extends UsuarioGenerico {
-
+	private Sede sede;
+	public Empleado(String elTypeUsuario, String elLogin, String laPass, String elNombre )
+	{
+		type = elTypeUsuario;
+		login = elLogin;
+		password = laPass;
+		nombre = elNombre;
+	}
+	public void cambiarEstadoVehiculo(List<Categoria> categorias, int numCarro, boolean nuevoEstado, String nuevaDescEstado)
+	{
+		List<Vehiculo> carros = sede.getCarrosSede(categorias);
+		Vehiculo carroACambiar = carros.get(numCarro);
+		carroACambiar.cambiarEstado(nuevoEstado, nuevaDescEstado);
+	}
+	
+	public String mostrarCarrosSede(List<Categoria> categorias)
+	{
+		List<Vehiculo> carros = sede.getCarrosSede(categorias);
+		String listado = "";
+		int numeroCarro = 0;
+		for (Vehiculo carro: carros)
+		{
+			listado = listado + Integer.toString(numeroCarro) + carro.toString() +"\n";
+		}
+		return listado;
+	}
+	
+	public void setSede(Sede laSede)
+	{
+		sede = laSede;
+	}
 }
