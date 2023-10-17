b1250e1484d72b3ff922c354fe8eb05ef247fdbb
MrRobot138 <123522773+MrRobot138@users.noreply.github.com>
2023-10-16 23:01:10 -0500

    Update AdminLocal.java

--- Entrega 3/proyecto1/src/modelo/AdminLocal.java
+++ Entrega 3/proyecto1/src/modelo/AdminLocal.java
@@ -1,5 +1,11 @@
 package modelo;
 
 public class AdminLocal extends UsuarioGenerico {
-
+	public AdminLocal(String elTypeUsuario, String elLogin, String laPass, String elNombre )
+	{
+		type = elTypeUsuario;
+		login = elLogin;
+		password = laPass;
+		nombre = elNombre;
+	}
 }
