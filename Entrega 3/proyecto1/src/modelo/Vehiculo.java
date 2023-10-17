1d383ad6a8bf957b33ece4b47aa290f8dce9784f
MrRobot138 <123522773+MrRobot138@users.noreply.github.com>
2023-10-16 20:15:26 -0500

    Update Vehiculo.java

--- Entrega 3/proyecto1/src/modelo/Vehiculo.java
+++ Entrega 3/proyecto1/src/modelo/Vehiculo.java
@@ -1,5 +1,77 @@
 package modelo;
 
 public class Vehiculo {
+	private String placa;
+	private String marca;
+	private String modelo;
+	private String color;
+	private String transmision;
+	private String sede;
+	private int capacidad;
+	private boolean estado;
+	private String descEstado;
+	
+	public Vehiculo(String laPlaca, String laMarca, String elModelo, String elColor, String laTransmision, int laCapacidad, boolean elEstado, String laDescEstado, String laSede)
+	{
+		placa = laPlaca;
+		marca =laMarca;
+		modelo = elModelo;
+		color = elColor;
+		transmision = laTransmision;
+		sede = laSede;
+		capacidad = laCapacidad;
+		estado = elEstado;
+		descEstado = laDescEstado;
+	}
+	
+	public String getPlaca() {
+		return placa;
+	}
 
+	public String getMarca() {
+		return marca;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public String getTransmision() {
+		return transmision;
+	}
+
+	public String getSede() {
+		return sede;
+	}
+
+	public int getCapacidad() {
+		return capacidad;
+	}
+
+	public boolean getEstado() {
+		return estado;
+	}
+
+	public String getDescEstado() {
+		return descEstado;
+	}
+
+	public void cambiarEstado(boolean nuevoEstado, String nuevaDescEstado)
+	{
+		this.estado = nuevoEstado;
+		this.descEstado = nuevaDescEstado;
+	}
+	public void cambiarSede(String nuevaSede)
+	{
+		this.sede = nuevaSede;
+	}
+	public String toString()
+	{
+		String carroStr = marca+" "+modelo+" placa: "+placa;
+		return carroStr;
+	}
 }
