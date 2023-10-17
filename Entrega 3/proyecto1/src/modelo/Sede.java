57061ed89b9a2c6e6224c83eced5942023e960e0
MrRobot138 <123522773+MrRobot138@users.noreply.github.com>
2023-10-16 20:14:24 -0500

    Update Sede.java

--- Entrega 3/proyecto1/src/modelo/Sede.java
+++ Entrega 3/proyecto1/src/modelo/Sede.java
@@ -1,15 +1,26 @@
 package modelo;
+import java.util.List;
+import java.util.ArrayList;
+
+import modelo.Vehiculo;
+import modelo.Categoria;
 
 public class Sede {
     private String nombre;
     private String ubicacion;
     private String horarios;
+    private List<Empleado> empleados;
 
     // Constructor
     public Sede(String nombre, String ubicacion, String horarios) {
         this.nombre = nombre;
         this.ubicacion = ubicacion;
         this.horarios = horarios;
+        empleados = new ArrayList<>();
+    }
+    public void anadirEmpleado(Empleado empleado)
+    {
+    	empleados.add(empleado);
     }
 
     // Métodos para acceder a los atributos
@@ -37,15 +48,21 @@ public class Sede {
     public void setHorarios(String horarios) {
         this.horarios = horarios;
     }
-
-    public static void main(String[] args) {
-        // Crear instancias de las sedes
-        Sede bogota = new Sede("Bogotá", "Cra. 11 #82-71", "08:00-17:00");
-        Sede cartagena = new Sede("Cartagena", "Cra. 8 #28-36", "08:00-17:00");
-        Sede medellin = new Sede("Medellín", "Cra. 13 #73-57", "08:00-17:00");
-
-        // Imprimir información de las sedes
-
+    
+    public ArrayList<Vehiculo> getCarrosSede(List<Categoria> categorias)
+    {
+    	ArrayList<Vehiculo> carros = new ArrayList<>();
+    	for (Categoria categoria : categorias)
+    	{
+    		List<Vehiculo> carrosCategoria = categoria.getCarros();
+    		for (Vehiculo carro : carrosCategoria)
+    		{
+    			if (carro.getSede() == this.nombre)
+    			{
+    				carros.add(carro);
+    			}
+    		}
+    	}
+    	return carros;
     }
 }
-
